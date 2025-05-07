f1f8b19379294174781e01ca5c784c4a8a155612
zkazi2004 <zaydkazi22@gmail.com>
2025-05-07 01:59:07 -0500

    Delete customer.sql

--- customer.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE Customer (
-    PersonalID INT PRIMARY KEY,
-    PreferredSalesRepID INT,
-    FOREIGN KEY (PersonalID) REFERENCES Person(PersonalID),
-    FOREIGN KEY (PreferredSalesRepID) REFERENCES Employee(PersonalID)
-);
