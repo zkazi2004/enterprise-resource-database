01fb3283da2e0a1c12c40c7f79f5a612b0cfde29
zkazi2004 <zaydkazi22@gmail.com>
2025-05-07 01:59:26 -0500

    Delete employee.sql

--- employee.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE Employee (
-    PersonalID INT PRIMARY KEY,
-    Rank VARCHAR(50),
-    Title VARCHAR(50),
-    SupervisorID INT,
-    FOREIGN KEY (PersonalID) REFERENCES Person(PersonalID),
-    FOREIGN KEY (SupervisorID) REFERENCES Employee(PersonalID)
-);
